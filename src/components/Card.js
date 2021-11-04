bf7ea723ccf22c493843e4db2ca7444738bfc713
sharath V <sharathkrml@gmail.com>
2021-11-04 11:58:50 +0530

    finished desktop design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import ellipsis from '../assets/images/icon-ellipsis.svg'
+import React from "react";
+import "./Card.css";
+import ellipsis from "../assets/images/icon-ellipsis.svg";
 function Card(props) {
-    return (
-        <div>
-            <div>
-                <h3>{props.title}</h3>
-                <img src={ellipsis} alt="ellipsis"/>
-            </div>
-            <h1>{props.current}hrs</h1>
-            <p>Last {props.timeframes} - {props.previous}hrs</p>
-        </div>
-    )
+  return (
+    <div className="card-container">
+      <div className="title-container">
+        <h3>{props.title}</h3>
+        <img className="ellipsis" src={ellipsis} alt="ellipsis" />
+      </div>
+      <div className="card-details">
+        <p className="current-time">{props.current}hrs</p>
+        <p className="previous-time">
+          Last {props.timeframes} - {props.previous}hrs
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
